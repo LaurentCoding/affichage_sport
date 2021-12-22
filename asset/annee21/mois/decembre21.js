7bd4fa297b73a5fbacc0077c24cd850982fc5841
Tazlocoding <tazlo2728@gmail.com>
2021-12-22 16:57:48 +0100

    Modification nom des fichiers js

---
+++ asset/annee21/mois/decembre21.js
@@ -0,0 +1,171 @@
+let decembre21 = [
+  {
+      "id": 11221,
+      "Date": "01/12/2021",
+      "Sport": "velo",
+      "Km": 27.86,
+      "Temps": "0h52",
+      "Moyenne": 32.15
+  },
+  {
+      "id": 21221,
+      "Date": "02/12/2021",
+      "Sport": "velo",
+      "Km": 22.08,
+      "Temps": "0h48",
+      "Moyenne": 27.6
+  },
+  {
+      "id": 31221,
+      "Date": "02/12/2021",
+      "Sport": "marche",
+      "Km": 4.93,
+      "Temps": "0h49",
+      "Moyenne": 5.97
+  },
+  {
+      "id": 41221,
+      "Date": "03/12/2021",
+      "Sport": "velo",
+      "Km": 25.27,
+      "Temps": "0h50",
+      "Moyenne": 30.32
+  },
+  {
+      "id": 51221,
+      "Date": "04/12/2021",
+      "Sport": "velo",
+      "Km": 30.27,
+      "Temps": "0h56",
+      "Moyenne": 32.43
+  },
+  {
+      "id": 61221,
+      "Date": "05/12/2021",
+      "Sport": "velo",
+      "Km": 29.54,
+      "Temps": "0h58",
+      "Moyenne": 30.56
+  },
+  {
+      "id": 71221,
+      "Date": "06/12/2021",
+      "Sport": "velo",
+      "Km": 26.15,
+      "Temps": "0h52",
+      "Moyenne": 30.17
+  },
+  {
+      "id": 81221,
+      "Date": "07/12/2021",
+      "Sport": "velo",
+      "Km": 26.99,
+      "Temps": "0h56",
+      "Moyenne": 28.92
+  },
+  {
+      "id": 91221,
+      "Date": "08/12/2021",
+      "Sport": "velo",
+      "Km": 29.16,
+      "Temps": "1h00",
+      "Moyenne": 29.16
+  },
+  {
+      "id": 101221,
+      "Date": "09/12/2021",
+      "Sport": "velo",
+      "Km": 32.57,
+      "Temps": "1h06",
+      "Moyenne": 29.61
+  },
+  {
+      "id": 111221,
+      "Date": "10/12/2021",
+      "Sport": "velo",
+      "Km": 21.75,
+      "Temps": "0h42",
+      "Moyenne": 31.07
+  },
+  {
+      "id": 121221,
+      "Date": "11/12/2021",
+      "Sport": "velo",
+      "Km": 29.08,
+      "Temps": "0h55",
+      "Moyenne": 31.72
+  },
+  {
+      "id": 131221,
+      "Date": "13/12/2021",
+      "Sport": "marche",
+      "Km": 9.4,
+      "Temps": "1h35",
+      "Moyenne": 5.94
+  },
+  {
+      "id": 141221,
+      "Date": "14/12/2021",
+      "Sport": "marche",
+      "Km": 12.96,
+      "Temps": "2h13",
+      "Moyenne": 5.85
+  },
+  {
+      "id": 151221,
+      "Date": "15/12/2021",
+      "Sport": "velo",
+      "Km": 41.74,
+      "Temps": "1h20",
+      "Moyenne": 31.3
+  },
+  {
+      "id": 161221,
+      "Date": "16/12/2021",
+      "Sport": "velo",
+      "Km": 33.19,
+      "Temps": "1h02",
+      "Moyenne": 32.12
+  },
+  {
+      "id": 171221,
+      "Date": "16/12/2021",
+      "Sport": "marche",
+      "Km": 5.35,
+      "Temps": "0h56",
+      "Moyenne": 5.73
+  },
+  {
+      "id": 181221,
+      "Date": "17/12/2021",
+      "Sport": "velo",
+      "Km": 33.63,
+      "Temps": "1h02",
+      "Moyenne": 32.55
+  },
+  {
+      "id": 191221,
+      "Date": "18/12/2021",
+      "Sport": "velo",
+      "Km": 34.23,
+      "Temps": "1h00",
+      "Moyenne": 34.23
+  },
+  {
+      "id": 201221,
+      "Date": "19/12/2021",
+      "Sport": "velo",
+      "Km": 41.49,
+      "Temps": "1h16",
+      "Moyenne": 32.76
+  },
+  {
+      "id": 211221,
+      "Date": "22/12/2021",
+      "Sport": "velo",
+      "Km": 43.65,
+      "Temps": "1h34",
+      "Moyenne": 27.86
+  }
+  
+]

af4205e9a06582d20464c9334ceac74e159de353
Tazlocoding <tazlo2728@gmail.com>
2022-01-10 11:39:25 +0100

    mise a jour des données début janvier

--- asset/annee22/mois/janvier22.js
+++ asset/annee22/mois/janvier22.js
@@ -22,5 +22,142 @@ let janvier22 = [
       "Km": 47.24,
       "Temps": "1h26",
       "Moyenne": 32.96
-  }
+  },
+  {
+      "id": 40122,
+      "Date": "07/01/2022",
+      "Sport": "vélo",
+      "Km": 45.25,
+      "Temps": "1h22",
+      "Moyenne": 33.11
+  },
+  {
+      "id": 50122,
+      "Date": "08/01/2022",
+      "Sport": "vélo",
+      "Km": 50.88,
+      "Temps": "1h30",
+      "Moyenne": 33.92
+  },
+  {
+      "id": 60122,
+      "Date": "09/01/2022",
+      "Sport": "vélo",
+      "Km": 46.59,
+      "Temps": "1h22",
+      "Moyenne": 34.09
+  },
+  {
+      "id": 70122,
+      "Date": "10/01/2022",
+      "Sport": "vélo",
+      "Km": 37.71,
+      "Temps": "1h06",
+      "Moyenne": 34.28
+  },
+  {
+      "id": 80122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 90122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 100122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 110122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 120122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 130122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 140122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 150122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 160122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 170122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 180122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 190122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 200122,
+      "Date": "/01/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  
 ]

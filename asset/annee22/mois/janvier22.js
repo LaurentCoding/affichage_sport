75217a258bb296cdfe876cb22a61dab7adedf2bc
Tazlocoding <tazlo2728@gmail.com>
2022-02-23 22:06:18 +0100

    Mise a jour debut février 2022

--- asset/annee22/mois/janvier22.js
+++ asset/annee22/mois/janvier22.js
@@ -57,107 +57,182 @@ let janvier22 = [
   },
   {
       "id": 80122,
-      "Date": "/01/2022",
+      "Date": "11/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
-      "Moyenne": 0
+      "Km": 45.81,
+      "Temps": "1h22",
+      "Moyenne": 33.52
   },
   {
       "id": 90122,
-      "Date": "/01/2022",
+      "Date": "12/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
-      "Moyenne": 0
+      "Km": 49.49,
+      "Temps": "1h30",
+      "Moyenne": 32.97
   },
   {
       "id": 100122,
-      "Date": "/01/2022",
+      "Date": "13/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
-      "Moyenne": 0
+      "Km": 52.82,
+      "Temps": "1h40",
+      "Moyenne": 31.69
   },
   {
       "id": 110122,
-      "Date": "/01/2022",
+      "Date": "14/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
-      "Moyenne": 0
+      "Km": 55.14,
+      "Temps": "1h40",
+      "Moyenne": 33.08
   },
   {
       "id": 120122,
-      "Date": "/01/2022",
+      "Date": "15/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
-      "Moyenne": 0
+      "Km": 60.98,
+      "Temps": "1h50",
+      "Moyenne": 33.26
   },
   {
       "id": 130122,
-      "Date": "/01/2022",
+      "Date": "16/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
-      "Moyenne": 0
+      "Km": 50.23,
+      "Temps": "1h30",
+      "Moyenne": 33.49
   },
   {
       "id": 140122,
-      "Date": "/01/2022",
+      "Date": "17/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
-      "Moyenne": 0
+      "Km": 47.95,
+      "Temps": "1h26",
+      "Moyenne": 33.45
   },
   {
       "id": 150122,
-      "Date": "/01/2022",
+      "Date": "18/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
-      "Moyenne": 0
+      "Km": 53.40,
+      "Temps": "1h36",
+      "Moyenne": 33.37
   },
   {
       "id": 160122,
-      "Date": "/01/2022",
+      "Date": "19/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
+      "Km": 54.60,
+      "Temps": "1h36",
       "Moyenne": 0
   },
   {
       "id": 170122,
-      "Date": "/01/2022",
+      "Date": "20/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
-      "Moyenne": 0
+      "Km": 51.05,
+      "Temps": "1h30",
+      "Moyenne": 33.39
   },
   {
       "id": 180122,
-      "Date": "/01/2022",
+      "Date": "21/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
-      "Moyenne": 0
+      "Km": 50.08,
+      "Temps": "1h30",
+      "Moyenne": 33.39
   },
   {
       "id": 190122,
-      "Date": "/01/2022",
+      "Date": "22/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
-      "Moyenne": 0
+      "Km": 53.42,
+      "Temps": "1h36",
+      "Moyenne": 33.39
   },
   {
       "id": 200122,
-      "Date": "/01/2022",
+      "Date": "23/01/2022",
       "Sport": "vélo",
-      "Km": 0,
-      "Temps": "",
-      "Moyenne": 0
+      "Km": 51.77,
+      "Temps": "1h30",
+      "Moyenne": 34.51
+  },
+  
+  {
+      "id": 210122,
+      "Date": "24/01/2022",
+      "Sport": "vélo",
+      "Km": 48.43,
+      "Temps": "1h25",
+      "Moyenne": 34.19
+  },
+  
+  {
+      "id": 220122,
+      "Date": "25/01/2022",
+      "Sport": "vélo",
+      "Km": 51.1,
+      "Temps": "1h30",
+      "Moyenne": 34.07
+  },
+  
+  {
+      "id": 230122,
+      "Date": "25/01/2022",
+      "Sport": "marche",
+      "Km": 11.59,
+      "Temps": "2h01",
+      "Moyenne": 5.75
+  },
+  {
+      "id": 240122,
+      "Date": "26/01/2022",
+      "Sport": "vélo",
+      "Km": 51.42,
+      "Temps": "1h30",
+      "Moyenne": 34.28
+  },
+  {
+      "id": 250122,
+      "Date": "27/01/2022",
+      "Sport": "vélo",
+      "Km": 50.52,
+      "Temps": "1h30",
+      "Moyenne": 33.68
+  },
+  {
+      "id": 260122,
+      "Date": "28/01/2022",
+      "Sport": "vélo",
+      "Km": 51.28,
+      "Temps": "1h32",
+      "Moyenne": 33.44
+  },
+  {
+      "id": 270122,
+      "Date": "29/01/2022",
+      "Sport": "vélo",
+      "Km": 57.25,
+      "Temps": "1h40",
+      "Moyenne": 34.35
+  },
+  {
+      "id": 280122,
+      "Date": "30/01/2022",
+      "Sport": "vélo",
+      "Km": 55.87,
+      "Temps": "1h36",
+      "Moyenne": 34.92
+  },
+  {
+      "id": 290122,
+      "Date": "31/01/2022",
+      "Sport": "vélo",
+      "Km": 64.83,
+      "Temps": "1h56",
+      "Moyenne": 33.53
   },
   
 ]
---
+++ asset/annee22/mois/mars22.js
@@ -0,0 +1,20 @@
+let mars22 = [
+  {
+      "id": 10322,
+      "Date": "/03/2022",
+      "Sport": "vélo",
+      "Km":0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  {
+      "id": 20322,
+      "Date": "/03/2022",
+      "Sport": "vélo",
+      "Km": 0,
+      "Temps": "",
+      "Moyenne": 0
+  },
+  
+  
+]
